14ce34f393fa80c9b22e7715122d7ae0cc6d5691
zakaria <zakariaselassi@gmail.com>
2021-03-01 16:13:57 +0100

    serie 1 finito

---
+++ 3.1.Test1/questions.js
@@ -0,0 +1,71 @@
+let tailleString = (texte) => {
+    return texte.length;
+}
+let remplaceECar = (texte) => {
+    let a = texte.indexOf("e")
+    let text = texte.substr(" ", a) + " " + texte.substr(a + 1, texte.length);
+    return text;
+}
+let concatString = (texte1, texte2) => {
+    return texte1 + texte2;
+}
+let afficherCar5 = (texte) => {
+    return texte[4];
+}
+let afficher9Car = (texte) => {
+    return texte.substring(0, 9);
+}
+let majusculeString = (texte) => {
+    texte = "UN AUTRE EXCELLENT EXERCICE";
+    return texte.toUpperCase();
+
+}
+let minusculeString = (texte) => {
+    return texte.toLowerCase();
+}
+let SupprEspaceString = (texte) => {
+
+    return texte.trim();
+}
+let IsString = (texte) => {
+    if (typeof texte) {
+        return true;
+    }
+
+}
+
+let AfficherExtensionString = (texte) => {
+    return texte.slice(15);
+}
+let NombreEspaceString = (texte) => {
+    return texte.split(" ").length - 1;
+}
+let InverseString = (texte) => {
+
+    let spliteur = texte.split("");
+    let reverse = spliteur.reverse();
+    let join = reverse.join("");
+    return join;
+}
+
+/**
+ * Exercices sur les nombres et les caluls mathématiques
+ */
+let calculPuissance = (x, y) => {
+    return Math.pow(x, y);
+}
+let valeurAbsolue = (nombre) => {
+    return Math.abs(nombre);
+}
+let valeurAbsolueArray = (array) => {
+    return array.map(Math.abs);
+}
+let sufaceCercle = (rayon) => {
+    return Math.round(rayon * rayon * Math.PI);
+}
+let hypothenuse = (ab, ac) => {
+    return Math.hypot(ab, ac);
+}
+let calculIMC = (poids, taille) => {
+    return console.log((poids / (taille * taille)).toFixed(2));
+}
