a01aaecf913b85e6f2675a489f4b3d26aff32398
zakaria <zakariaselassi@gmail.com>
2021-03-02 10:07:15 +0100

    Serie 2 Finito

--- 3.2.Test2/questions.js
+++ 3.2.Test2/questions.js
@@ -1,43 +1,59 @@
-let CreationTableauLangages =  () => {
+let CreationTableauLangages = () => {
+    let arr = ["Html", "CSS", "Java", "PHP"];
+    return arr;
 
 }
 
-let CreationTableauNombres =  () => {
+let CreationTableauNombres = () => {
+    let arr = [];
+
+    for (let i = 0; i < 6; i++) {
+        arr.push(i);
+    }
+    return arr;
+}
+
+let RemplacementElement = (langages) => {
+    langages.splice(2, 1, "Javascript");
+
+    return langages;
+}
+
+let AjoutElementLangages = (langages) => {
+    langages.push("Ruby", "Python");
+    return langages;
+}
+
+let AjoutElementNombres = (nombres) => {
+    nombres.unshift(-2, -1);
+    return nombres;
+}
+
+let SuppressionPremierElement = (langages) => {
+    langages.shift();
+
+    return langages;
+}
+
+let SuppressionDernierElement = (langages) => {
+    langages.pop();
+    return langages;
+}
+
+let ConversionChaineTableau = (reseaux_sociaux_chaine) => {
+
+    return reseaux_sociaux_chaine.split(",");
 
 }
 
-let RemplacementElement =  (langages) => {
-
+let ConversionTableauChaine = (langages) => {
+    return langages.join(",");
 }
 
-let AjoutElementLangages =  (langages) => {
-
+let TriTableau = (reseaux_sociaux) => {
+    return reseaux_sociaux.sort();
 }
 
-let AjoutElementNombres =  (nombres) => {
-
-}
-
-let SuppressionPremierElement =  (langages) => {
-
-}
-
-let SuppressionDernierElement =  (langages) => {
-
-}
-
-let ConversionChaineTableau =  (reseaux_sociaux_chaine) => {
-
-}
-
-let ConversionTableauChaine =  (langages) => {
-
-}
-
-let TriTableau =  (reseaux_sociaux) => {
-
-}
-
-let InversionTableau =  (reseaux_sociaux) =>{
-
+let InversionTableau = (reseaux_sociaux) => {
+    return reseaux_sociaux.reverse();
 }
